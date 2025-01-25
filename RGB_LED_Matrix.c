6352bec523d77df4dff8e5f666be62a928f2b32e
Jorge Palma <jorgepalmaconceicao@gmail.com>
2025-01-25 02:38:34 -0300

    Deixando a matriz de led funcional

--- RGB_LED_Matrix.c
+++ RGB_LED_Matrix.c
@@ -1,127 +1,146 @@
+/*
+*  GRUPO 6 - SUBGRUPO 4 - EMBARCATECH
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "pico/bootrom.h"
+
 #include "include/keypad.h"
 
-#define RED_LED_RGB 13
-#define GREEN_LED_RGB 11
-#define BLUE_LED_RGB 12
+//arquivo .pio
+#include "pio_matrix.pio.h"
 
-#define MATRIZ_LED_PIN 7
+//número de LEDs
+#define NUM_PIXELS 25
+
+//pino de saída
+#define OUT_PIN 7
 
 // Define os pinos do teclado
 uint8_t row_pins[ROWS] = {16, 17, 18, 19};
 uint8_t col_pins[COLS] = {20, 4, 9, 8};
 
-void configure_pwm(uint gpio_pin) {
-    
-    gpio_set_function(gpio_pin, GPIO_FUNC_PWM);
-    // Obtém o slice de PWM do pino
-    uint slice_num = pwm_gpio_to_slice_num(gpio_pin);
-    // Habilita o PWM no slice
-    pwm_set_enabled(slice_num, true);
+PIO pio = pio0; 
+bool ok;
+uint16_t i;
+uint32_t valor_led;
+double r = 0.0, b = 0.0 , g = 0.0;
+uint sm = 0;
+
+double ligar_todos_os_leds[25] =   {1.0, 1.0, 1.0, 1.0, 1.0,
+                            1.0, 1.0, 1.0, 1.0, 1.0, 
+                            1.0, 1.0, 1.0, 1.0, 1.0,
+                            1.0, 1.0, 1.0, 1.0, 1.0,
+                            1.0, 1.0, 1.0, 1.0, 1.0};
+
+                            //vetor para criar imagem na matriz de led - 1
+double desliga_tudo[25] =   {0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0, 
+                            0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0};
+
+
+//imprimir valor binário
+void imprimir_binario(int num) {
+ int i;
+ for (i = 31; i >= 0; i--) {
+  (num & (1 << i)) ? printf("1") : printf("0");
+ }
 }
 
-void set_pwm_duty(uint gpio_pin, uint8_t value) {
-    // Obtém o slice e canal do pino
-    uint slice_num = pwm_gpio_to_slice_num(gpio_pin);
-    uint channel = pwm_gpio_to_channel(gpio_pin);
-
-    // Mapeia o valor de 0-255 para 0-65535 (16 bits para resolução PWM)
-    uint16_t duty_cycle = (value * 65535) / 255;
-
-    // Define o duty cycle do canal
-    pwm_set_chan_level(slice_num, channel, duty_cycle);
-}
-
-void setup_gpio()
+//rotina para definição da intensidade de cores do led
+uint32_t matrix_rgb(double b, double r, double g)
 {
-    // gpio_init(RED_LED_RGB);             
-    // gpio_set_dir(RED_LED_RGB, GPIO_OUT);
-    // gpio_init(GREEN_LED_RGB);
-    // gpio_set_dir(GREEN_LED_RGB, GPIO_OUT);
-    // gpio_init(BLUE_LED_RGB);
-    // gpio_set_dir(BLUE_LED_RGB, GPIO_OUT);
-    
-    configure_pwm(RED_LED_RGB);
-    configure_pwm(GREEN_LED_RGB);
-    configure_pwm(BLUE_LED_RGB);
-    gpio_init(MATRIZ_LED_PIN);
-    gpio_set_dir(MATRIZ_LED_PIN, GPIO_OUT);
+  unsigned char R, G, B;
+  R = r * 255;
+  G = g * 255;
+  B = b * 255;
+  return (G << 24) | (R << 16) | (B << 8);
 }
 
-// Função para executar ações com base na tecla pressionada
+void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm){
+
+    for (int16_t i = 0; i < NUM_PIXELS; i++) {
+        valor_led = matrix_rgb(desenho[24-i], desenho[24-i], desenho[24-i]);
+        pio_sm_put_blocking(pio, sm, valor_led);
+        //imprimir_binario(valor_led);
+    }
+    printf("clock set to %ld\n", clock_get_hz(clk_sys));
+}
+
+
 void escolher_acao(char key) 
 {
     switch (key) 
     {
         case '1': 
-            gpio_put(RED_LED_RGB, 0);
-            break; // Ação para a tecla '1'
+            break; 
         case '2': 
-            break; // Ação para a tecla '2'
+            break;
         case '3': 
-            break; // Ação para a tecla '3'
+            break;
         case '4': 
-            break; // Ação para a tecla '4'
+            break;
         case '5': 
-            break; // Ação para a tecla '5'
+            break;
         case '6': 
-            break; // Ação para a tecla '6'
+            break;
         case '7': 
-            break; // Ação para a tecla '7'
+            break;
         case '8': 
-            break; // Ação para a tecla '8'
-        case '9': 
-            break; // Ação para a tecla '9'
+            desenho_pio(ligar_todos_os_leds, valor_led, pio, sm);
+            break;
+        case '9':
+            break;
         case '0': 
-            // Liga o led RGB
-            set_pwm_duty(RED_LED_RGB, 255);   // Vermelho no máximo
-            set_pwm_duty(GREEN_LED_RGB, 128); // Verde em 50%
-            set_pwm_duty(BLUE_LED_RGB, 64);   // Azul em 25%
-
-            sleep_ms(500);
-
-            set_pwm_duty(RED_LED_RGB, 50);   // Vermelho no máximo
-            set_pwm_duty(GREEN_LED_RGB, 20); // Verde em 50%
-            set_pwm_duty(BLUE_LED_RGB, 255);   // Azul em 25%
-
-            sleep_ms(500);
-
-            set_pwm_duty(RED_LED_RGB, 255);   // Vermelho no máximo
-            set_pwm_duty(GREEN_LED_RGB, 128); // Verde em 50%
-            set_pwm_duty(BLUE_LED_RGB, 64);   // Azul em 25%
-
-            // gpio_put(RED_LED_RGB, 1);
-            break; // Ação para a tecla '0'
+            // Liga o led RGB    
+            break;
         case 'A': 
-            break; // Ação para a tecla 'A'
+            desenho_pio(desliga_tudo, valor_led, pio, sm); 
+            break;
         case 'B': 
-            break; // Ação para a tecla 'B'
+            break;
         case 'C': 
-            break; // Ação para a tecla 'C'
+            break;
         case 'D': 
-            break; // Ação para a tecla 'D'
+            break;
         case '*': 
-            break; // Ação para a tecla '*'
+            break;
         case '#': 
-            break; // Ação para a tecla '#'
+            break;
         default: 
+            desenho_pio(desliga_tudo, valor_led, pio, sm);
             break; // Ação padrão para qualquer outra tecla
     }
 }
 
-int main() 
+//função principal
+int main()
 {
-    
+    //coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
+    ok = set_sys_clock_khz(128000, false);
+
     stdio_init_all();
-    setup_gpio();
-    printf("Inicializando teclado matricial...\n");
+
+    printf("iniciando a transmissão PIO");
+    if (ok) printf("clock set to %ld\n", clock_get_hz(clk_sys));
+
+    //configurações da PIO
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, OUT_PIN);
 
     // Inicializa o teclado
     init_keypad(row_pins, col_pins);
 
-    while (1) 
+    while (true) 
     {
         char key = scan_keypad(row_pins, col_pins);
         if (key != '\0') // Se uma tecla foi pressionada
@@ -129,6 +148,7 @@ int main()
             printf("Tecla pressionada: %c\n", key);
             escolher_acao(key); // Executa a ação associada
         }
+        sleep_ms(300);  // debounce
     }
 
     return 0;
